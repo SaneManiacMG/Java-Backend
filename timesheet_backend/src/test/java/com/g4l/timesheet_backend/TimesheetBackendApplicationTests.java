42de0b592daa18c4e224ff79b3726547cde0e8b0
Mogomotsi Tshwagong <mmoroane@hotmail.com>
2024-01-12 23:17:49 +0200

    Code generation

---
+++ timesheet_backend/src/test/java/com/g4l/timesheet_backend/TimesheetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g4l.timesheet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
