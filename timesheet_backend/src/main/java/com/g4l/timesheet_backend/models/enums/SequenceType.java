111f46bd39bd1c9c962c5bdc7c4461df72bcf984
Mogomotsi Tshwagong <mmoroane@hotmail.com>
2024-01-30 08:10:52 +0200

    Created sequence generator

---
+++ timesheet_backend/src/main/java/com/g4l/timesheet_backend/models/enums/SequenceType.java
@@ -0,0 +1,9 @@
+package com.g4l.timesheet_backend.models.enums;
+
+public enum SequenceType {
+    LOGBOOK_ID,
+    CONSULTANT_ID,
+    MANAGER_ID,
+    CLIENT_ID,
+    TEAM_ID
+}
---
+++ timesheet_backend/src/main/java/com/g4l/timesheet_backend/utils/SequenceGenerator.java
@@ -0,0 +1,44 @@
+package com.g4l.timesheet_backend.utils;
+
+import java.time.LocalDate;
+import java.util.Random;
+import org.springframework.stereotype.Service;
+import com.g4l.timesheet_backend.models.enums.SequenceType;
+
+@Service
+public abstract class SequenceGenerator {
+    public static String generateSequence(SequenceType sequenceType) {
+        String sequence = sequenceTypeString(sequenceType) + dateString() + randomNumber();
+        return sequence;
+    }
+
+    private static String sequenceTypeString(SequenceType sequenceType) {
+        switch (sequenceType) {
+            case CLIENT_ID:
+                return "CL";
+            case LOGBOOK_ID:
+                return "LO";
+            case MANAGER_ID:
+                return "MA";
+            case TEAM_ID:
+                return "TE";
+            case CONSULTANT_ID:
+                return "CO";
+            default:
+                break;
+        }
+        return "";
+    }
+
+    private static String dateString() {
+        LocalDate localDate = LocalDate.now();
+        String date = localDate.toString();
+        return date;
+    }
+
+    private static String randomNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(9999 - 1000) + 1000;
+        return String.valueOf(randomNumber);
+    }
+}
